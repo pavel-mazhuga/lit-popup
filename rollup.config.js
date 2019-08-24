334f528d193945102d7105dece6ee4c73e8d15b9
pavel-mazhuga <pavelmazhuga@yandex.ru>
2019-08-24 22:40:51 +0700

    add css

--- rollup.config.js
+++ rollup.config.js
@@ -2,16 +2,12 @@ import typescript from 'rollup-plugin-typescript2';
 import resolve from 'rollup-plugin-node-resolve';
 import { terser } from 'rollup-plugin-terser';
 import { eslint } from 'rollup-plugin-eslint';
+import css from 'rollup-plugin-css-porter';
 import pkg from './package.json';
 
-const extensions = [
-    '.js', '.jsx', '.ts', '.tsx',
-];
+const extensions = ['.js', '.jsx', '.ts', '.tsx'];
 
-const external = [
-    ...Object.keys(pkg.dependencies || {}),
-    ...Object.keys(pkg.peerDependencies || {}),
-];
+const external = [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})];
 
 export default [
     {
@@ -30,6 +26,10 @@ export default [
         external,
         plugins: [
             resolve({ extensions }),
+            css({
+                raw: 'dist/css/lit-popup.css',
+                minified: 'dist/css/lit-popup.min.css',
+            }),
             eslint(),
             typescript({
                 typescript: require('typescript'),
