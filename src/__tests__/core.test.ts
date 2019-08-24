a6c2cc5f34841bfd1c0e9408aad2874cb42cb9c2
pavel-mazhuga <pavelmazhuga@yandex.ru>
2019-08-24 23:04:56 +0700

    fix tests

--- src/__tests__/core.test.ts
+++ src/__tests__/core.test.ts
@@ -1,9 +1,9 @@
-import LitPopup from '../index';
+import LitPopup from '../lit-popup';
 import { withPrefix } from '../utils';
 
 const name = 'test';
 const element = document.createElement('div');
-element.dataset.litModal = name;
+element.dataset.litPopup = name;
 
 beforeEach(() => {
     document.body.appendChild(element);
@@ -13,28 +13,30 @@ afterEach(() => {
     document.body.innerHTML = '';
 });
 
-it('throws when no DOM element found', () => {
-    element.remove();
-    expect(() => new LitPopup(name)).toThrow(withPrefix('Element not found.'));
-});
+describe('lit-popup inits properly', () => {
+    it('throws when no DOM element found', () => {
+        element.remove();
+        expect(() => new LitPopup(name)).toThrow(withPrefix('Element not found.'));
+    });
 
-it('inits properly', () => {
-    LitPopup.prototype.init = jest.fn();
+    it('inits properly', () => {
+        (LitPopup.prototype as any).init = jest.fn();
 
-    const openButton = document.createElement('button');
-    openButton.dataset.litModalOpen = name;
-    document.body.appendChild(openButton);
+        const openButton = document.createElement('button');
+        openButton.dataset.litPopupOpen = name;
+        document.body.appendChild(openButton);
 
-    const closeButton = document.createElement('button');
-    closeButton.dataset.litModalClose = name;
-    document.body.appendChild(closeButton);
+        const closeButton = document.createElement('button');
+        closeButton.dataset.litPopupClose = name;
+        document.body.appendChild(closeButton);
 
-    const instance = new LitPopup(name);
+        const instance = new LitPopup(name);
 
-    expect(instance.init).toHaveBeenCalledTimes(1);
-    expect(instance.el).toEqual(element);
-    expect(instance.isOpen).toEqual(false);
-    expect(instance.innerContainer).toBeNull();
-    expect((instance as any).openButtons.length).toEqual(1);
-    expect((instance as any).closeButtons.length).toEqual(1);
+        expect((instance as any).init).toHaveBeenCalledTimes(1);
+        expect(instance.el).toEqual(element);
+        expect(instance.isOpen).toEqual(false);
+        expect(instance.innerContainer).toBeNull();
+        expect((instance as any).openButtons.length).toEqual(1);
+        expect((instance as any).closeButtons.length).toEqual(1);
+    });
 });
