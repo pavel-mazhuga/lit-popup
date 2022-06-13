0b7105828d383c9f2bf227b3f31b2f96654d0a53
pavel-mazhuga <pavelmazhuga@yandex.ru>
2022-06-13 19:18:59 +0700

    update packages, move to esm by default

--- dist/esm/plugins/keydown.d.ts
+++
@@ -1,3 +0,0 @@
-import { LitPopupInterface, PluginDestroyer } from '../index';
-declare const _default: (instance: LitPopupInterface) => PluginDestroyer;
-export default _default;
--- rollup.config.js
+++ rollup.config.js
@@ -1,36 +1,28 @@
 import typescript from 'rollup-plugin-typescript2';
-import resolve from 'rollup-plugin-node-resolve';
+import resolve from '@rollup/plugin-node-resolve';
 import { terser } from 'rollup-plugin-terser';
 import { eslint } from 'rollup-plugin-eslint';
-// import css from 'rollup-plugin-css-porter';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import pkg from './package.json';
 
 const extensions = ['.js', '.jsx', '.ts', '.tsx'];
 
-// const external = [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})];
-
 export default {
     input: 'src/index.ts',
     output: [
+        // {
+        //     file: pkg.main,
+        //     format: 'iife',
+        //     name: 'LitPopup',
+        // },
         {
             file: pkg.main,
-            format: 'iife',
-            name: 'LitPopup',
-        },
-        {
-            file: pkg.module,
             format: 'esm',
         },
     ],
-    // external,
     plugins: [
         peerDepsExternal(),
         resolve({ extensions }),
-        // css({
-        //     raw: 'dist/css/lit-popup.css',
-        //     minified: 'dist/css/lit-popup.min.css',
-        // }),
         eslint(),
         typescript({
             typescript: require('typescript'),
